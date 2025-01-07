a9d1e6c6e0b1d9ef1907225a6085bad7d3960ff9
ggnosw45x <129354202+ggnosw45x@users.noreply.github.com>
2025-01-07 06:00:01 -0500

    Add files via upload

---
+++ configss/_def_main_.py
@@ -0,0 +1,13 @@
+from gotas.chattext import *
+from pyrogram import filters
+from pyrogram import Client, filters
+import requests
+import re
+import random
+from random import randint
+from luhn import *
+from gotas.botones import *
+from rank import *
+import time
+import json
+import string
