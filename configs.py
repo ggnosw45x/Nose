8702b6946c4e9c1edeae9cd1d5d01689c7da18f1
ggnosw45x <129354202+ggnosw45x@users.noreply.github.com>
2025-01-07 06:10:55 -0500

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv('API_ID','21187534')) # Coloque el API ID del Telegram Cuenta
     API_HASH = getenv('API_HASH','50a7a5d0da3b6ca49da582c84732a8ef') # Coloque el API HASH del Telegram Cuenta
-    BOT_TOKEN = getenv('BOT_TOKEN','5799910541:AAH3ZlLlfNu64rixpRaEf5xmvh0AslY5TH0') #Coloque el TOKEN DEL BOT de @BotFather
+    BOT_TOKEN = getenv('BOT_TOKEN','7807495947:AAGvrDQJ0WpnjXLFPhkDsdOB6ql2m7rN0hc') #Coloque el TOKEN DEL BOT de @BotFather
  #En las comillas tiene que ir tu token del bot 
 
 config = Config()
